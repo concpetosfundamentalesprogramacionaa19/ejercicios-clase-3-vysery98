86eb44ab7de877241ed14d0586490b104bbfcd1f
vysery98 <49170787+vysery98@users.noreply.github.com>
2019-04-25 11:40:30 -0500

    Update Clase2.java

--- Ejercicios3/src/paquete2/Clase2.java
+++ Ejercicios3/src/paquete2/Clase2.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
 
 /**
  *
- * @author reroes
+ * @author vysery98
  */
 public class Clase2 {
 
