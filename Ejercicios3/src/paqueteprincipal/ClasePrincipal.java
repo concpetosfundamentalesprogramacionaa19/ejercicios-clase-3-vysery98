947faad1bf9198877ae7b47b5a1d06b0e4b814ca
vysery98 <49170787+vysery98@users.noreply.github.com>
2019-04-25 11:44:54 -0500

    Update ClasePrincipal.java

--- Ejercicios3/src/paqueteprincipal/ClasePrincipal.java
+++ Ejercicios3/src/paqueteprincipal/ClasePrincipal.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paqueteprincipal;
 import paquete1.*;
 /** 
  *
- * @author reroes
+ * @author vysery98
  */
 public class ClasePrincipal {
    
